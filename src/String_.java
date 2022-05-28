b8ebf62843bbe71e11e63b3a3e07ce6aa156e4e1
16072 <1607268776@qq.com>
2022-05-28 16:32:02 +0800

    Initial commit

---
+++ src/String_.java
@@ -0,0 +1,8 @@
+public class String_ {
+    public static void main(String[] args) {
+        //不是基本类型 创建的出来的是对象
+        String str = ""; // 也是一个对象 相当于String str = new String("");
+        
+
+    }
+}
